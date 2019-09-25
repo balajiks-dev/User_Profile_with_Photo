f3f3dbfdede8877b9b65670e46aa5aebf81fe49d
balajisundaram <balajiks006@gmail.com>
2019-09-25 18:38:05 +0530

    Updating the Row bottom of the Display Name

--- lib/main.dart
+++ lib/main.dart
@@ -40,58 +40,123 @@ class _MyHomePageState extends State<MyHomePage> {
             Card(
               color: Colors.amberAccent,
               shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(30.0),),
-              margin: EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 0.0),
+              margin: EdgeInsets.fromLTRB(10.0, 10.0, 10.0, 0.0),
               child: ListTile(
                 leading: new CircleAvatar(
                   radius: 30,
                   backgroundImage: NetworkImage('https://miro.medium.com/max/400/1*gH1iKXJH8T12LIqhboZWEA.png'),
                 ),
                 title: new Text('Cole Baranet',style: TextStyle(color: Colors.green),),
-                subtitle: new Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  children: <Widget>[
-
-                  ],
+                subtitle: Padding(padding: EdgeInsets.only(
+                  top:10),
+                     child: new Row(
+                       // mainAxisSize: MainAxisSize.max,
+                       //mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                       children: <Widget>[
+                         new Icon(Icons.account_circle),
+                         new Text('8,432',style: TextStyle(fontSize: 11,fontWeight: FontWeight.w300),),
+                         new Padding(
+                           padding: EdgeInsets.only(left: 10.0),
+                           child: Row(
+                             children: <Widget>[
+                               new Icon(Icons.timer),
+                               new Text('10:20 am',style: TextStyle(fontSize: 11,fontWeight: FontWeight.w300),),
+                             ],
+                           ),
+                         ),
+                         new Padding(
+                             padding: EdgeInsets.only(left: 10.0,right: 5.0),
+                             child: Row(
+                               children: <Widget>[
+                                 new Icon(Icons.calendar_today),
+                                 new Text('12-08-2019',style: TextStyle(fontSize: 11,fontWeight: FontWeight.w300),),
+                               ],
+                             )
+                         ),
+                       ],
+                     ),
                 ),
               ),
             ),
             Card(
               color: Colors.amberAccent,
               shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(30.0),),
-              margin: EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 0.0),
-              child: ListTile(
-//                leading: new ClipRRect(
-//                  borderRadius: new BorderRadius.circular(10.0),
-//                  child: new Image.network('https://miro.medium.com/max/400/1*gH1iKXJH8T12LIqhboZWEA.png'),
-//                ),
-               leading: new CircleAvatar(
-                radius: 30,
-                backgroundImage: NetworkImage('https://miro.medium.com/max/400/1*gH1iKXJH8T12LIqhboZWEA.png'),
-               ),
-                title: new Text('Cole Baranet',style: TextStyle(color: Colors.green),),
-                subtitle: new Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  children: <Widget>[
-
-                  ],
-                ),
-              ),
-            ),
-            Card(
-              color: Colors.amberAccent,
-              shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(30.0),),
-              margin: EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 0.0),
+              margin: EdgeInsets.fromLTRB(10.0, 10.0, 10.0, 0.0),
               child: ListTile(
                 leading: new CircleAvatar(
                   radius: 30,
                   backgroundImage: NetworkImage('https://miro.medium.com/max/400/1*gH1iKXJH8T12LIqhboZWEA.png'),
                 ),
                 title: new Text('Cole Baranet',style: TextStyle(color: Colors.green),),
-                subtitle: new Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  children: <Widget>[
-
-                  ],
+                subtitle: Padding(padding: EdgeInsets.only(
+                    top:10),
+                  child: new Row(
+                    // mainAxisSize: MainAxisSize.max,
+                    //mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: <Widget>[
+                      new Icon(Icons.account_circle),
+                      new Text('8,432',style: TextStyle(fontSize: 11,fontWeight: FontWeight.w300),),
+                      new Padding(
+                        padding: EdgeInsets.only(left: 10.0),
+                        child: Row(
+                          children: <Widget>[
+                            new Icon(Icons.timer),
+                            new Text('10:20 am',style: TextStyle(fontSize: 11,fontWeight: FontWeight.w300),),
+                          ],
+                        ),
+                      ),
+                      new Padding(
+                          padding: EdgeInsets.only(left: 10.0,right: 5.0),
+                          child: Row(
+                            children: <Widget>[
+                              new Icon(Icons.calendar_today),
+                              new Text('12-08-2019',style: TextStyle(fontSize: 11,fontWeight: FontWeight.w300),),
+                            ],
+                          )
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+            Card(
+              color: Colors.amberAccent,
+              shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(30.0),),
+              margin: EdgeInsets.fromLTRB(10.0, 10.0, 10.0, 0.0),
+              child: ListTile(
+                leading: new CircleAvatar(
+                  radius: 30,
+                  backgroundImage: NetworkImage('https://miro.medium.com/max/400/1*gH1iKXJH8T12LIqhboZWEA.png'),
+                ),
+                title: new Text('Cole Baranet',style: TextStyle(color: Colors.green),),
+                subtitle: Padding(padding: EdgeInsets.only(
+                    top:10),
+                  child: new Row(
+                    // mainAxisSize: MainAxisSize.max,
+                    //mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: <Widget>[
+                      new Icon(Icons.account_circle),
+                      new Text('8,432',style: TextStyle(fontSize: 11,fontWeight: FontWeight.w300),),
+                      new Padding(
+                        padding: EdgeInsets.only(left: 10.0),
+                        child: Row(
+                          children: <Widget>[
+                            new Icon(Icons.timer),
+                            new Text('10:20 am',style: TextStyle(fontSize: 11,fontWeight: FontWeight.w300),),
+                          ],
+                        ),
+                      ),
+                      new Padding(
+                          padding: EdgeInsets.only(left: 10.0,right: 5.0),
+                          child: Row(
+                            children: <Widget>[
+                              new Icon(Icons.calendar_today),
+                              new Text('12-08-2019',style: TextStyle(fontSize: 11,fontWeight: FontWeight.w300),),
+                            ],
+                          )
+                      ),
+                    ],
+                  ),
                 ),
               ),
             ),
